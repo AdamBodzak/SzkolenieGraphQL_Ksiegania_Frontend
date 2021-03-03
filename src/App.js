a93c12fb58b1046845df68806cc01f6cb9d33edd
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-03 15:58:11 +0100

    Chang basic page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import React from "react";
-import AuthorsPage from "./pages/AuthorsPage";
-import { Box, Divider, Flex, Heading, Link } from "@chakra-ui/react";
-import { Routes, Route, Link as RouterrLink } from "react-router-dom";
-import UsersPage from "./pages/UsersPage";
+import { Routes, Route, Link as RouterLink } from "react-router-dom";
+
 import BooksPage from "./pages/BooksPage";
+import AuthorsPage from "./pages/AuthorsPage";
+import UsersPage from "./pages/UsersPage";
+import { Flex, Box, Divider, Heading, Link } from "@chakra-ui/react";
 
 export default function App() {
   return (
     <Flex direction="column" align="center" width="75%" mx="auto">
-      <Flex  align="center" justify="space-between" w="100%">
-        <Link to="/" as={RouterrLink}>
+      <Flex align="center" justifyContent="space-between" w="100%" mx="5">
+        <Link to="/" as={RouterLink}>
           <Heading as="h1">Personal Library</Heading>
         </Link>
-        <Flex>
-        <Link to="books/" as={RouterrLink}>
-            <Box as="span">Books</Box>
+        <Box d="flex">
+          <Link to="/" as={RouterLink}>
+            <h1>Books</h1>
           </Link>
           <Divider orientation="vertical" />
-          <Link to="/" as={RouterrLink}>
-            <Box as="span">Authors</Box>
+          <Link to="/authors" as={RouterLink}>
+            <h1>Authors</h1>
           </Link>
           <Divider orientation="vertical" />
-          <Link to="users/" as={RouterrLink}>
-            <Box as="span">Users</Box>
+          <Link to="/users" as={RouterLink}>
+            <h1>Users</h1>
           </Link>
-        </Flex>
+        </Box>
       </Flex>
       <Routes>
-        <Route path="/" element={<AuthorsPage />} />
-        <Route path="/users" element={<UsersPage />} />
-        <Route path="/books" element={<BooksPage />} />
+        <Route path="/" element={<BooksPage />} />
+        <Route path="authors/" element={<AuthorsPage />} />
+        <Route path="users/" element={<UsersPage />} />
       </Routes>
     </Flex>
   );
