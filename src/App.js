15a8e524d44d88d1247e64181af14d057d005efa
Adam Bodzak <bodzak.adam@gmail.com>
2021-03-03 15:56:03 +0100

    Add BooksPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AuthorsPage from "./pages/AuthorsPage";
 import { Box, Divider, Flex, Heading, Link } from "@chakra-ui/react";
 import { Routes, Route, Link as RouterrLink } from "react-router-dom";
 import UsersPage from "./pages/UsersPage";
+import BooksPage from "./pages/BooksPage";
 
 export default function App() {
   return (
@@ -12,6 +13,10 @@ export default function App() {
           <Heading as="h1">Personal Library</Heading>
         </Link>
         <Flex>
+        <Link to="books/" as={RouterrLink}>
+            <Box as="span">Books</Box>
+          </Link>
+          <Divider orientation="vertical" />
           <Link to="/" as={RouterrLink}>
             <Box as="span">Authors</Box>
           </Link>
@@ -24,6 +29,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<AuthorsPage />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="/books" element={<BooksPage />} />
       </Routes>
     </Flex>
   );
---
+++ src/components/Book.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Flex, Heading, Image } from "@chakra-ui/react";
+
+export default function Book({ id, title, cover, author }) {
+  return (
+    <Flex
+      mt="5"
+      w="100%"
+      border="1px"
+      borderRadius="md"
+      borderColor="gray.200"
+      overflow="hidden"
+      bg="gray.50"
+    >
+      <Image size="100px" objectFit="cover" src={cover.url} />
+      <Flex direction="column" mx="2" justify="center">
+        <Heading as="h3" size="md" color="gray.700">
+          {title}
+        </Heading>
+        <Heading as="h4" size="sm" color="gray.400">
+          {author.name}
+        </Heading>
+      </Flex>
+    </Flex>
+  );
+}
